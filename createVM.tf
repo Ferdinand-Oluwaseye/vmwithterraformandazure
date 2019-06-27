7ec2ccba89e04427c8d9f4158925ea1ebbc59b58
Ferdinand-Oluwaseye <ronferd2012@gmail.com>
2019-06-27 13:46:07 +0100

    Updated terraform vm script to add a network security group and a public ip

--- createVM.tf
+++ createVM.tf
@@ -23,17 +23,47 @@ resource "azurerm_subnet" "internal" {
 	address_prefix = "10.0.2.0/24"
 }
 
+
+resource "azurerm_public_ip" "main" {
+	name = "${var.prefix}-publicip"
+	location = "${azurerm_resource_group.main.location}"	
+	resource_group_name = "${azurerm_resource_group.main.name}"
+	allocation_method = "Dynamic"
+
+}
+
+resource "azurerm_network_security_group" "main"{
+	name = "${var.prefix}-nsg"
+	location = "${azurerm_resource_group.main.location}"
+	resource_group_name = "${azurerm_resource_group.main.name}"
+	
+	security_rule {
+		name = "SSH"
+		priority = "400"
+		direction = "Inbound"
+		access = "Allow"
+		protocol = "Tcp"
+		source_port_range = "*"
+		destination_port_range = "22"
+		source_address_prefix = "*"
+		destination_address_prefix = "*"
+	}
+}
+
+
+
 resource "azurerm_network_interface" "main" {
 	name = "${var.prefix}-nic"
 	location = "${azurerm_resource_group.main.location}"
 	resource_group_name = "${azurerm_resource_group.main.name}"
-
+	network_security_group_id = "${azurerm_network_security_group.main.id}"
+	
 	ip_configuration {
 		name = "testconfiguration1"
 		subnet_id = "${azurerm_subnet.internal.id}"
 		private_ip_address_allocation = "Dynamic"
-
-
+		public_ip_address_id = "${azurerm_public_ip.main.id}"
+		
 	}
 }
 
